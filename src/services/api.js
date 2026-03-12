d4b93b8571b8db46b33ad78e7e6b23389a3607b3
jordonmassengill <67432176+jordonmassengill@users.noreply.github.com>
2026-03-12 14:40:24 -0700

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:5000/api';
+const API_URL = 'https://tattoo-finder-backend-production.up.railway.app';
 
 // Create axios instance
 const api = axios.create({
